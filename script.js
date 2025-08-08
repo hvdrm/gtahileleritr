d6dd6419c2d30b80fb25f5fef7032cd78b3ea596
btfhavakuvvetleri <dorukberkea@gmail.com>
2025-08-09 00:31:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+function copyCode(text) {
+  navigator.clipboard.writeText(text).then(() => {
+    alert("Kod kopyalandı: " + text);
+  });
+}
+
+function filterHiles() {
+  const input = document.getElementById('searchInput');
+  const filter = input.value.toLowerCase();
+  const hileList = document.getElementById('hileList');
+  const sections = hileList.getElementsByTagName('section');
+
+  for (let i = 0; i < sections.length; i++) {
+    const lis = sections[i].getElementsByTagName('li');
+    let sectionVisible = false;
+
+    for (let j = 0; j < lis.length; j++) {
+      const text = lis[j].textContent || lis[j].innerText;
+
+      if (text.toLowerCase().indexOf(filter) > -1) {
+        lis[j].style.display = '';
+        sectionVisible = true;
+      } else {
+        lis[j].style.display = 'none';
+      }
+    }
+ 
+    
+    sections[i].style.display = sectionVisible ? '' : 'none';
+  }
+}
+
+
